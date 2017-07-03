1e87e21b015113f48e3a26ad6d0ac6dfaec2838c
Dennis Hedegaard <dennis@dhedegaard.dk>
2017-07-03 16:55:12 +0200

    run.sh: Changed indentation

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 for fname in $(
-    find . \
-      -maxdepth 1 \
-      -type d \
-      -name "e*"
+  find . \
+    -maxdepth 1 \
+    -type d \
+    -name "e*"
 );
 do
-    echo "*** $fname ***"
-    cd $fname
-    cargo run -q
-    cd ..
+  echo "*** $fname ***"
+  cd $fname
+  cargo run -q
+  cd ..
 done
