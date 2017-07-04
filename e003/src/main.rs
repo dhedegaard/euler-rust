ae074201859acb4cccc54d64d2aaf60320a13e45
Dennis Hedegaard <dennis@dhedegaard.dk>
2017-07-04 21:10:53 +0200

    e003: Use a bitvector to make it a lot faster :)

--- e003/src/main.rs
+++ e003/src/main.rs
@@ -1,32 +1,30 @@
-use std::collections::HashMap;
+extern crate bit_vec;
+use bit_vec::BitVec;
 use std::time::SystemTime;
 
 /// Returns a vector of prime numbers up to the cap, the numbers are not sorted in any way.
 fn eratosthenes(cap: u64) -> Vec<u64> {
     let cap_sqrt = (cap as f64).sqrt() as u64;
     // Start by allocating a map with all integer values up to cap with a value of true.
-    let mut map = HashMap::with_capacity(cap_sqrt as usize);
-    for i in 2..cap_sqrt {
-        map.insert(i, true);
-    }
+    let mut vec = BitVec::from_elem(cap_sqrt as usize, true);
     // For each 
     for i in 2..cap_sqrt {
         // Ignore positions already marked as false.
-        if !map.get(&i).unwrap() {
+        if !vec.get(i as usize).unwrap() {
             continue
         }
         // Otherwise, mark positions after this one, step by "i" each time.
         let mut j = i * 2;
         while j < cap_sqrt {
-            map.insert(j, false);
+            vec.set(j as usize, false);
             j += i;
         }
     }
     // Take all the true values and aggregate them in a vector.
     let mut result = vec!();
-    for (key, value) in &map {
-        if *value {
-            result.push(*key)
+    for key in 2..cap_sqrt {
+        if vec.get(key as usize).unwrap() {
+            result.push(key)
         }
     }
     result
