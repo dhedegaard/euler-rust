c96ccd5b4fc9bf207d35bdf8d66fbe149fe98240
Dennis Hedegaard <dennis@dhedegaard.dk>
2017-07-05 19:33:28 +0200

    e001: fixed formatting

--- e001/src/main.rs
+++ e001/src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
-    println!(
-        "{}",
-        (1..1000).filter(|e| e % 3 == 0 || e % 5 == 0).fold(
-            0,
-            |acc, e| {
-                acc + e
-            },
-        )
-    );
+    println!("{}", (1..1000).filter(|e| e % 3 == 0 || e % 5 == 0).fold(0, |acc, e| { acc + e }));
 }
